fbcdef0e4024767f2ee3490b3fe3c43877123ce1
Danilsonunes <danilsonunes@gmail.com>
2020-10-08 00:42:25 -0300

    ahora si las rutas del backend listas

--- back-end/src/controllers/stock.controller.js
+++ back-end/src/controllers/stock.controller.js
@@ -1,23 +1,28 @@
 ﻿const db = require("../config/database");
 
-
 // ==> Método responsável por listar todos os 'Stocks':
 
 exports.listAllStock = async (req, res) => {
+  const polo = req.params.polo;
   const allStock = await db.query(
-    "SELECT stock FROM estoque WHERE polo = 'AM - MANAUS'"
+    "SELECT stock FROM estoque WHERE polo LIKE '%' || $1 || '%'",
+    [polo]
   );
   const manausStock = await db.query(
-    "SELECT count(maquinas) maquinas_vendidas_6_meses FROM atendimentos WHERE fecha > '2020-04-09' AND polo = 'AM - MANAUS'"
+    "SELECT count(maquinas) maquinas_vendidas_6_meses FROM atendimentos WHERE fecha > '2020-04-09' AND polo LIKE '%' || $1 || '%'",
+    [polo]
   );
   const porcentaje6Meses = await db.query(
-    "SELECT count(maquinas) / 180 AS promedio_X_dia FROM atendimentos WHERE fecha > '2020-04-09' AND polo = 'AM - MANAUS'"
+    "SELECT count(maquinas) / 180 AS promedio_X_dia FROM atendimentos WHERE fecha > '2020-04-09' AND polo LIKE '%' || $1 || '%'",
+    [polo]
   );
   const normal = await db.query(
-    "SELECT count(maquinas) / 180 * 14 AS estoque_normal_14_dias FROM atendimentos WHERE fecha > '2020-04-09' AND polo = 'AM - MANAUS'"
+    "SELECT count(maquinas) / 180 * 14 AS estoque_normal_14_dias FROM atendimentos WHERE fecha > '2020-04-09' AND polo LIKE '%' || $1 || '%'",
+    [polo]
   );
   const dias = await db.query(
-    "SELECT stock / (SELECT COUNT(*) / 180 AS estoque_normal_14_dias FROM atendimentos WHERE fecha > '2020-04-09' AND polo = 'AM - MANAUS') AS dias_de_estoque FROM estoque WHERE polo = 'AM - MANAUS'"
+    "SELECT stock / (SELECT COUNT(*) / 180 AS estoque_normal_14_dias FROM atendimentos WHERE fecha > '2020-04-09' AND polo LIKE '%' || $1 || '%') AS dias_de_estoque FROM estoque WHERE polo LIKE '%' || $1 || '%'",
+    [polo]
   );
   res.status(200).send({
     all: allStock.rows,
@@ -27,16 +32,3 @@ exports.listAllStock = async (req, res) => {
     remainingDays: dias.rows,
   });
 };
-
-function StatusColor({ count }) {
-  const statusDoTrocinhoLaQueNaoLembroJa = (count) => {
-    if (count < 14) { return 'red' } 
-    if (count > 14) { return 'green' }
-    return 'gray'
-  }
-  const colorClass = statusDoTrocinhoLaQueNaoLembroJa(count)
-
-  return (
-    <div className={colorClass}>{count}</div>
-  )
-}
